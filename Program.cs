c770f0a6c7e427f36ed319740cd48f3dfc5b811e
Yimy A. <yimy.net@gmail.com>
2026-04-13 19:24:14 -0400

    load project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+
+// use openApi
+// use Swag
+// create api with security aproach
